dcb4cad45d9585aac1b3a5611e4a3c1aaa98b63d
Sergei Barbarash <sgt.sgt@gmail.com>
2023-05-17 19:15:36 +0300

    running daemons locally

--- teamgramd/docker/entrypoint.sh
+++ teamgramd/docker/entrypoint.sh
@@ -7,7 +7,7 @@
 export TEAMGRAM_HOST=${TEAMGRAM_HOST:-"0.0.0.0"}
 export ETCD_URL=${ETCD_URL:-"etcd:2379"}
 export REDIS_HOST=${REDIS_HOST:-"redis:6379"}
-export KAFKA_HOST=${KAFKA_HOST:-"kafka:9092"}
+export KAFKA_HOST=${KAFKA_HOST:-"kafka:9094"}
 export MYSQL_URI=${MYSQL_URI:-"teamgram:teamgram@tcp(mysql:3306)/teamgram?charset=utf8mb4"}
 export MINIO_URI=${MINIO_URI:-"minio:9000"}
 export MINIO_KEY=${MINIO_KEY:-"minio"}
